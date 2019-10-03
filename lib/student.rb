c980542c919b59bb8f82f929740e5230bce7c00f
Cody Rickman <cody@creativemindstudios.com>
2019-10-03 17:17:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,29 +1,49 @@
+require 'sqlite3'
+require_relative '../lib/student'
+
+DB = {:conn => SQLite3::Database.new("db/students.db")}
+
 class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    student = Student.new
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    return student
   end
 
   def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+      SELECT * FROM students
+      SQL
+    results = DB[:conn].execute(sql)
+    students = []
+    results.each do |row|
+      students << Student.new_from_db(row)
+    end
+    return students
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT * FROM students WHERE name = ?
+      SQL
+    row = DB[:conn].execute(sql, name)
+    student = Student.new_from_db(row)
+    return student
   end
-  
+
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
-  
+
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
